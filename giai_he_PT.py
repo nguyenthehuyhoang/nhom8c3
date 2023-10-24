a3e7d22572910b3e5f2a8d812095da7f78e7587b
Tran-Van-Hieu <145484433+Tran-Van-Hieu@users.noreply.github.com>
2023-10-24 22:00:31 +0700

    Update giai_he_PT.py

--- giai_he_PT.py
+++ giai_he_PT.py
@@ -1,82 +1,105 @@
-#bài này giải hệ phương trình x+2y=5 và 3x+4y =6
-# Yêu cầu hoàn chỉnh lại đoạn code
-#để có 1 app giải hệ phương trình có n phương trình n ẩn
-
 import numpy as np
+import tkinter as tk
+from tkinter import ttk
 
-def solve_linear_equations(nhapsopt, an):
+def solve_linear_equations(coeff_matrix, constants):
     try:
-        A = np.array(nhapsopt)# biểu diễn các hệ số ẩn thành ma trận
-        B = np.array(an)# biểu diễn các hệ số tự do thành ma trận
-        A_inv = np.linalg.inv(A)##  tính ma trận nghịch đảo của ma trận A
-        X = np.dot(A_inv, B)#  tính tích của ma trận nghịch đảo A_inv và ma trận B
+        A = np.array(coeff_matrix)
+        B = np.array(constants)
+        A_inv = np.linalg.inv(A)
+        X = np.dot(A_inv, B)
         return X
-    except np.linalg.LinAlgError: ## nếu ma trận không thể nghịch đảo
+    except np.linalg.LinAlgError:
         return None
 
-def main():
-    nhapsopt = None
-    while nhapsopt is None:
-        try:
-            nhapsopt = int(input("Nhập số lượng phương trình: "))
-            if nhapsopt <= 0:
-                print("Số lượng phương trình phải lớn hơn 0. Vui lòng nhập lại.")
-                nhapsopt = None
-        except ValueError:
-            print("Giá trị không hợp lệ. Vui lòng nhập lại.")
-    an = None
-    while an is None:
-        try:
-            an = int(input("Nhập số lượng ẩn: "))
-            if an <= 0:
-                print("Số lượng phương trình phải lớn hơn 0. Vui lòng nhập lại.")
-                an = None
-        except ValueError:
-            print("Giá trị không hợp lệ. Vui lòng nhập lại.")
-
-
+def solve_equations():
+    nhapsopt = int(nhapsopt_entry.get())
+    an = int(an_entry.get())
+    
     hesoan_matrix = []
     hesotudo_matrix = []
 
-    print("Nhập hệ số các phương trình:")
     for i in range(nhapsopt):
         matrix_tam = []
         for j in range(an):
-            hesoan = None
-            while hesoan is None:
-                try:
-                    hesoan = float(input(f"Nhập hệ số của ẩn x{j+1} trong phương trình {i+1}: "))
-                    if hesoan <= 0:
-                        print("Số lượng phương trình phải lớn hơn 0. Vui lòng nhập lại.")
-                        hesoan = None
-                except ValueError:
-                    print("Giá trị không hợp lệ. Vui lòng nhập lại.")
-
-
+            hesoan = float(hesoan_entries[i][j].get())
             matrix_tam.append(hesoan)
         hesoan_matrix.append(matrix_tam)
-        hesotudo = None
-        while hesotudo is None:
-            try:
-                hesotudo = float(input(f"Nhập hệ số tự do trong phương trình {i + 1}: "))
-                if hesoan <= 0:
-                    print("Số lượng phương trình phải lớn hơn 0. Vui lòng nhập lại.")
-                    hesotudo = None
-            except ValueError:
-                print("Giá trị không hợp lệ. Vui lòng nhập lại.")
-
-
+        hesotudo = float(hesotudo_entries[i].get())
         hesotudo_matrix.append(hesotudo)
 
-    nghiem= solve_linear_equations(hesoan_matrix, hesotudo)
+    nghiem = solve_linear_equations(hesoan_matrix, hesotudo_matrix)
 
     if nghiem is not None:
-        print("Nghiệm của hệ phương trình:")
+        result_label.config(text="Nghiệm của hệ phương trình:")
         for i, value in enumerate(nghiem):
-            print(f"x{i+1} = {value}")
+            nghiem_labels[i].config(text=f"x{i+1} = {value}")
     else:
-        print("Hệ phương trình vô nghiệm hoặc vô số nghiệm.")
+        result_label.config(text="Hệ phương trình vô nghiệm hoặc vô số nghiệm.")
 
-if __name__ == "__main__":
-    main()
+root = tk.Tk()
+root.title("Giải Hệ Phương Trình Tuyến Tính")
+
+frame1 = ttk.Frame(root)
+frame1.pack(padx=10, pady=10, fill='both')
+
+nhapsopt_label = ttk.Label(frame1, text="Số lượng phương trình:")
+nhapsopt_label.grid(row=0, column=0)
+nhapsopt_entry = ttk.Entry(frame1)
+nhapsopt_entry.grid(row=0, column=1)
+nhapsopt_entry.insert(0, "2")
+
+an_label = ttk.Label(frame1, text="Số lượng ẩn:")
+an_label.grid(row=1, column=0)
+an_entry = ttk.Entry(frame1)
+an_entry.grid(row=1, column=1)
+an_entry.insert(0, "2")
+
+frame2 = ttk.Frame(root)
+frame2.pack(padx=10, pady=10, fill='both')
+
+hesoan_labels = []
+hesotudo_labels = []
+
+for i in range(2):
+    hesoan_label = ttk.Label(frame2, text=f"Hệ số ẩn x{i+1}:")
+    hesoan_label.grid(row=i, column=0)
+    hesoan_labels.append(hesoan_label)
+
+    hesotudo_label = ttk.Label(frame2, text=f"Hệ số tự do phương trình {i+1}:")
+    hesotudo_label.grid(row=i, column=2)
+    hesotudo_labels.append(hesotudo_label)
+
+hesoan_entries = []
+hesotudo_entries = []
+
+for i in range(2):
+    hesoan_row = []
+    for j in range(2):
+        hesoan_entry = ttk.Entry(frame2)
+        hesoan_entry.grid(row=i, column=1)
+        hesoan_row.append(hesoan_entry)
+    hesoan_entries.append(hesoan_row)
+
+    hesotudo_entry = ttk.Entry(frame2)
+    hesotudo_entry.grid(row=i, column=3)
+    hesotudo_entries.append(hesotudo_entry)
+
+solve_button = ttk.Button(root, text="Giải", command=solve_equations)
+solve_button.pack(pady=10)
+
+frame3 = ttk.Frame(root)
+frame3.pack(padx=10, pady=10, fill='both')
+
+result_label = ttk.Label(frame3, text="")
+result_label.grid(row=0, column=0)
+
+nghiem_labels = []
+
+for i in range(2):
+    nghiem_label = ttk.Label(frame3, text="")
+    nghiem_label.grid(row=i+1, column=0)
+    nghiem_labels.append(nghiem_label)
+
+root.mainloop()
 code đúng rồi không phải sửa ạ
