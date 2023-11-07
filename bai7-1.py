6697af9bfd3d248b30307f189240f0dbe9dacb3c
Trần Anh Tú <120768787+tenlatu01042002@users.noreply.github.com>
2023-11-07 12:44:48 +0700

    Update bai7-1.py

--- bai7-1.py
+++ bai7-1.py
@@ -3,39 +3,37 @@ import tkinter as tk
 from tkinter import filedialog
 from tkinter import messagebox
 
-def open_image():
-    file_path = filedialog.askopenfilename()
-    if file_path:
-        image = cv2.imread(file_path)
-        return image
-    return None
-
-def blur_image():
-    image = open_image()
-    if image is not None:
-        x, y, width, height = 100, 270, 150, 150
-        roi = image[y:y+height, x:x+width]
-        ksize = (21, 21)
-        sigma = 0
-        blurred_roi = cv2.GaussianBlur(roi, ksize, sigma)
-        image[y:y+height, x:x+width] = blurred_roi
-        cv2.imwrite('blurred_image.jpg', image)
-        messagebox.showinfo("Success", "Image blurred and saved as 'blurred_image.jpg'")
-        cv2.imshow('Original Image', image)
+class ImageBlurringApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Image Blurring")
         
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+        # Tạo nút để mở bức ảnh
+        open_button = tk.Button(self.root, text="Open Image", command=self.open_image)
+        open_button.pack()
+        
+        # Tạo nút để làm mờ bức ảnh
+        blur_button = tk.Button(self.root, text="Blur Image", command=self.blur_image)
+        blur_button.pack()
 
-# Tạo cửa sổ giao diện
-root = tk.Tk()
-root.title("Image Blurring")
+    def open_image(self):
+        file_path = filedialog.askopenfilename()
+        if file_path:
+            self.image = cv2.imread(file_path)
+            if self.image is not None:
+                messagebox.showinfo("Success", "Image loaded successfully")
+            else:
+                messagebox.showerror("Error", "Failed to load image")
 
-# Tạo nút để mở bức ảnh
-#open_button = tk.Button(root, text="Open Image", command=open_image)
-#open_button.pack()
+    def blur_image(self):
+        if hasattr(self, 'image'):
+            ksize = (21, 21)
+            sigma = 0
+            self.image = cv2.GaussianBlur(self.image, ksize, sigma)
+            cv2.imwrite('blurred_image.jpg', self.image)
+            messagebox.showinfo("Success", "Image blurred and saved as 'blurred_image.jpg'")
 
-# Tạo nút để làm mờ bức ảnh
-blur_button = tk.Button(root, text="Blur Image", command=blur_image)
-blur_button.pack()
-
-root.mainloop()
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = ImageBlurringApp(root)
+    root.mainloop()
